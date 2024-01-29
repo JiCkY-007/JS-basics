0136671e152de3a52c57eaaa304cabd27379ef90
JiCkY-007 <63316956+JiCkY-007@users.noreply.github.com>
2024-01-29 16:22:35 +0000

    datatyoes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all js code as newer Version
+
+// => alert("heyyyyy")// we are using nodejs not browser
+
+let name = "abhigyan" // string
+let age = 21 // number
+let isLoggedIn = false //boolean
+
+
+//==> PRIMATIVE DATA TYPES
+
+// number => 2^53
+// string => "" ''  
+// boolean => true/false
+// null => standalone value // Represents a "empty" value
+// undefined => value is not assigned 
+// symbol => unique
+
+// Object 
+
+console.log(typeof null)
+
+// Null ek object hai 
+// undefined undefined hai
