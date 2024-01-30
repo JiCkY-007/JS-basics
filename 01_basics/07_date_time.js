348676122a03b5a47772abfbbab9476f01da7b67
JiCkY-007 <63316956+JiCkY-007@users.noreply.github.com>
2024-01-30 12:00:54 +0000

    date and time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,22 @@
+// Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023 , 0, 23)
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+// interview question
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
