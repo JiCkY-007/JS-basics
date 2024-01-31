7bd94b020555b83ab4dacf5619599dc84ba77262
JiCkY-007 <63316956+JiCkY-007@users.noreply.github.com>
2024-01-31 07:01:13 +0000

    study objects in depth part again

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,44 @@
+// Singleton : jab bhi hum literals ki tarah declare krte hai toh singleton/
+// nahi bnanega lekin agar constructor se baenga toh singleton banega
+
+// object Literals 
+
+//object.create singleton method
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
